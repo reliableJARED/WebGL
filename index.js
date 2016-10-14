bdaa1f86ff7e0dfefb7c830b9b45fa503675ee27
jared <jaredn@microcal.com>
2016-10-14 17:17:30 -0400

    finished add/rmv players and getting everything sync'd up

--- index.js
+++ index.js
@@ -40,14 +40,48 @@ app.use(serveStatic(__dirname + '/static/ammo.js/builds/'))
 var physicsWorld;
 var gravityConstant = -1; //-9.8
 var rigidBodies =  new Array();
-var rigidBodiesIndex = new Array();//holds info about world objects.  Sent to newly connected clients so that they can build the world
+var rigidBodiesIndex = new Object();//holds info about world objects.  Sent to newly connected clients so that they can build the world.  Similar to ridgidBodies but includes height, width, depth, color, object type.
+									//info that is only needed when a newly connected player first builds the world
+									
+/*
+var RigidBodyAccess = (function() {
+  var rigidBodies =  new Array();
+ 
+ function addObj(obj) {
+    rigidBodies.push(obj);
+  }
+  function deleteObj(index){
+	  rigidBodies.slice(index,1);
+  }
+  return {
+    remove: function(idx) {
+      deleteObj(idx);
+    },
+    add: function(obj) {
+      addObj(obj);
+    },
+    getAll: function() {
+      return rigidBodies;
+    },
+	count:function(){
+	  return rigidBodies.length;
+	}
+  };   
+})();
+*/
+
+									
+									
+var PlayerIndex = new Object();//matches a player's ID to their rigidBodiesIndex object
 var collisionConfiguration;
 var dispatcher;
 var broadphase;
 var solver;
-var transformAux1 = new Ammo.btTransform();
+var transformAux1 = new Ammo.btTransform();//reusable transform object
 var EndTimeLastPhysicsStep = Date.now(); 
 var PhysicsSimStarted = false;
+var vector3Aux1 = new Ammo.btVector3(); //reusable vector object
+var quaternionAux1 = new Ammo.btQuaternion(); //reusable quaternion object
 
 function initPhysics() {
 	
@@ -80,108 +114,101 @@ function initPhysics() {
 
 function createObjects() {
 		
-		/*************CREATE CUBE  *********/ 
-		var w = 2;
-		var h =2;
-		var d =2;
-		var mass = 5;
-		var x =1;
-		var y =10;
-		var z=1;
-		var Rx =0;
-		var Ry=0;
-		var Rz=0;
-		
-		var pos = new Ammo.btVector3(x,y,z);		
-		var quat = new Ammo.btQuaternion(Rx,Ry,Rz,1);
-
-		var cube = createPhysicalCube(w,h,d,mass,pos,quat);
-		
-		//add to our physics object holder
-		rigidBodies.push( cube );
-		
-		//add cube to physics world
-		physicsWorld.addRigidBody( cube );
-		
-		//create a parallel array that holds construction info about our object
-		var lookupID = 'id'+cube.ptr.toString();
-
-		rigidBodiesIndex[lookupID] = {
-				id:lookupID,
-				x:x, 
-				y:y, 
-				z:z, 
-				Rx:Rx, 
-				Ry:Ry, 
-				Rz:Rz, 
-				w:w, 
-				h:h, 
-				d:d, 
-				mass:mass, 
-			   shape:'box',
-			   color: "rgb(100%, 0%, 0%)"
-			};
-
-
 		/*************CREATE GROUND  *********/ 
-		 w = 20;
-		 h = 20;
-		 d = 1;
-		var massg = 0; //zero mass makes objects static.  Objects can hit them but they dont move or fall 
-		 y = 1
+		var groundObjBlueprint = {
+			mass : 0, //zero mass makes objects static.  Objects can hit them but they dont move or fall 
+			width : 20,
+			height : 1,
+			depth : 20,
+			shape:'box',
+			color: "rgb(0%, 100%, 0%)",
+			x: 0,
+			y: 0,
+			z: 0,
+			Rx: 0,
+			Ry: 0,
+			Rz: 0
+		}
 		
-		 pos = new Ammo.btVector3(x,y,z);		
-		 quat = new Ammo.btQuaternion(Rx,Ry,Rz,1);
 
-		var ground = createPhysicalCube(w,h,d,massg,pos,quat);
-		
-		//add to our physics object holder
-		rigidBodies.push( ground );
-		
-		//add cube to physics world
-		physicsWorld.addRigidBody( ground );
-		
-		//create a parallel array that holds construction info about our object
-		lookupID = 'id'+ground.ptr.toString();
+		//build the object
+		var ground = createPhysicalCube(groundObjBlueprint);
 
-		rigidBodiesIndex[lookupID] = {
-				id:lookupID,
-				x:x, 
-				y:y, 
-				z:z, 
-				Rx:Rx, 
-				Ry:Ry, 
-				Rz:Rz, 
-				w:w, 
-				h:h, 
-				d:d, 
-				mass:mass, 
-			   shape:'box',
-			   color: "rgb(50%, 50%, 0%)"
-			};
-
+		//add ground our physics object holder
+		rigidBodies.push( ground.physics );
+		
+		//add ground to physics world
+		physicsWorld.addRigidBody( ground.physics );
+		
+		//add ground to our index used to update clients about objects that have moved
+		/*IMPORTANT: AddToRigidBodiesIndex expects that obj.physics is an Ammo object.  NOT the values sent used in the blueprint to build the object*/
+		AddToRigidBodiesIndex(ground);
 }
 
-function createPhysicalCube (sx, sy, sz, mass, pos, quat){
+function AddToRigidBodiesIndex(obj){
+	
+	//rigidBodiesIndex holds construction info about our object
+	//it is used for new players to construct the current world state
+	//Ammo assigns a uniqueID number to every object which can be found in the 'ptr' property
+	
+	//assign our objects loc/rot to our reusable transform object
+	obj.physics.getMotionState().getWorldTransform( transformAux1 );
+	var loc = transformAux1.getOrigin();//position
+	var rot = transformAux1.getRotation();//orientation
+	
+	rigidBodiesIndex[obj.id] = {
+				id:obj.id,
+				x:loc.x(), 
+				y:loc.y(), 
+				z:loc.z(), 
+				Rx:rot.x(), 
+				Ry:rot.y(), 
+				Rz:rot.z(), 
+				w:obj.width, 
+				h:obj.height, 
+				d:obj.depth, 
+				mass:obj.mass, 
+			    shape:obj.shape,
+			    color:obj.color
+			};
+}
 
+
+//the object returned from createPhysicalCube has places the Ammo object under the property 'physics'
+//the x,y,z,Rx,Ry,Rz props are deleted and a new prop 'physics' is created.
+function createPhysicalCube (blueprint){
+	
+	//need error handling and default values
+	var sx = blueprint.width
+	var sy =  blueprint.height
+	var sz =  blueprint.depth
+	var mass =  blueprint.mass
+	
+	/*set the position of our physics object using our reusable vector object*/
+	vector3Aux1.setX(blueprint.x);
+	vector3Aux1.setY(blueprint.y);
+	vector3Aux1.setZ(blueprint.z);
+	
+	/*set the orientation of our physics object using our reusable quaternion object*/
+	quaternionAux1.setEulerZYX(blueprint.Rz,blueprint.Ry,blueprint.Rx);
+		
 	var physicsShape = new Ammo.btBoxShape(new Ammo.btVector3( sx * 0.5, sy * 0.5, sz * 0.5 ) );
 	
 	//set the collision margin, don't use zero, default is typically 0.04
 	physicsShape.setMargin(0.04);
 	
-	/*set the location of our physics object based on where the graphics object is*/
+	/* use a transform to apply the loc/orient of our new physics object in world space using our reusable transform object*/
 	//btTransform() supports rigid transforms with only translation and rotation and no scaling/shear.
-	var transform = new Ammo.btTransform();
-	transform.setIdentity();
+	transformAux1.setIdentity();
 	
 	//setOrigin() is for location
-	transform.setOrigin( pos );
+	transformAux1.setOrigin( vector3Aux1 );
     
 	//setRotation() is for Orientation
-	transform.setRotation( quat );
+	transformAux1.setRotation( quaternionAux1 );
 	
 	//set the motion state and inertia of our object
-	var motionState = new Ammo.btDefaultMotionState( transform );
+	var motionState = new Ammo.btDefaultMotionState( transformAux1 );
 	
 	var localInertia = new Ammo.btVector3( 0, 0, 0 );
 	
@@ -192,20 +219,40 @@ function createPhysicalCube (sx, sy, sz, mass, pos, quat){
 	
 	//build our ridgidBody
 	var Cube = new Ammo.btRigidBody( rbInfo );
+	
+	//clean up our blueprint to be returned as the final object
+	delete blueprint.x;
+	delete blueprint.y;
+	delete blueprint.z;
+	delete blueprint.Rx;
+	delete blueprint.Ry;
+	delete blueprint.Rz;
+	
+	//add new prop holding our object
+	blueprint.physics = Cube;
+	
+	//assign the objects uniqueID
+	blueprint.id = 'id'+Cube.ptr.toString();
 
-	return Cube;
+	//return our object which is now ready to be added to the world
+	return blueprint;
 }
 
 function updatePhysics( deltaTime ) {
 	
-	//this will hold a tree of objects that need to be updated due to Physics simulation
-	//the structure is that ObjectUpdateJSON has a bunch of properties which are the ID's of the objects.  Each object branch contains the new XYZ, rotation X,Y,Z for the objects. 
-	var ObjectUpdateJSON = {};
+	//similar to the rigidBodiesIndex but only used to hold updates per frame, not the WHOLE world
+	var ObjectUpdateJSON = new Object();
+
+	//this function will create a tree of objects that need to be updated due to Physics simulation
+	//the structure is that ObjectUpdateJSON has a bunch of properties which are the ID's of the objects.  
+	//Each object branch will be an object that changed and it's new XYZ, rotation X,Y,Z for the objects. 
+	//clients hold a matching representation of the physics object using the ID's in their presented graphics.
+	
 
 	/* http://www.bulletphysics.org/mediawiki-1.5.8/index.php/Stepping_The_World */
 	physicsWorld.stepSimulation( deltaTime,10);//Bullet maintains an internal clock, in order to keep the actual length of ticks constant
 
-	// Update rigid bodies
+	// check for rigidbodies that are in motion (changed) since last stepSimulation
 	for ( var i = 0; i < rigidBodies.length; i++ ) {
 		var obj = rigidBodies[ i ];
 		var ms =  obj.getMotionState();
@@ -214,8 +261,6 @@ function updatePhysics( deltaTime ) {
 				
 				//Bullet calls getWorldTransform with a reference to the variable it wants you to fill with transform information
 				ms.getWorldTransform( transformAux1 );//note: transformAux1 =  Ammo.btTransform();
-				
-				 var lookupID = 'id'+obj.ptr.toString();
 		
 				//get the physical location of our object
 				var p = transformAux1.getOrigin();
@@ -229,8 +274,10 @@ function updatePhysics( deltaTime ) {
 				var Ry = q.y();	
 				var Rz = q.z();	
 				
+				var ObjectID = 'id'+obj.ptr.toString();
+				
 				//add this object to our update JSON to be sent to all clients
-				ObjectUpdateJSON[lookupID] = {x:x, y:y, z:z, Rx:Rx, Ry:Ry, Rz:Rz};
+				ObjectUpdateJSON[ObjectID] = {x:x, y:y, z:z, Rx:Rx, Ry:Ry, Rz:Rz};
 				
 				/*IMPORTANT!
 				rigidBodiesIndex[] is used for new connections only.  But it should stay up to date with where objects are now.  It is inefficient to constantly update this since we already know on the server
@@ -250,11 +297,13 @@ function updatePhysics( deltaTime ) {
 	
 	//when I used process.nextTick() was preventing clients from being able to connect, recursive loop of the physics world was created and no other process would run
 	//process.nextTick(render);
-	
+
 	//Send the list of objects to be updated
 	/* CONSIDERATION! should this JSON have some time stamp associated? */
 	io.emit('update', ObjectUpdateJSON );
 };
+
+
 
 function render() {
 	   var deltaTime = (Date.now()/1000) - (EndTimeLastPhysicsStep/1000);
@@ -266,31 +315,110 @@ function BuildWorldStateForNewConnection(){
 
 	//http://stackoverflow.com/questions/35769707/socket-io-loses-data-on-server-side
 	var world = new Array();
-	
+
 	for(var i = 0; i < rigidBodies.length; i++){
+		//Try/Catch is here because ridgidBodies length can be affected by other functions.  synchronized locking not setup yet
+		var lookUp = 'id'+rigidBodies[i].ptr.toString();
 		
-		var obj = rigidBodies[i]
-		
-		var lookupID = 'id'+obj.ptr.toString();
-		
-		obj.getMotionState().getWorldTransform( transformAux1 )
-		
-		//get the physical orientation and location of our object
-		var p = transformAux1.getOrigin();
-		rigidBodiesIndex[lookupID].x =  p.x();	
-		rigidBodiesIndex[lookupID].y = p.y()
-		rigidBodiesIndex[lookupID].z = p.z()		
+		try{
+			var obj =  rigidBodies[i]
+			
+			obj.getMotionState().getWorldTransform( transformAux1 )
+			
+			//get the physical orientation and location of our object
+			var p = transformAux1.getOrigin();
+			rigidBodiesIndex[lookUp].x = p.x();	
+			rigidBodiesIndex[lookUp].y = p.y();
+			rigidBodiesIndex[lookUp].z = p.z();
 				
-		var q = transformAux1.getRotation();
-		rigidBodiesIndex[lookupID].Rx = q.x();
-		rigidBodiesIndex[lookupID].Ry = q.y();
-		rigidBodiesIndex[lookupID].Rz = q.z();
+			var q = transformAux1.getRotation();
+			rigidBodiesIndex[lookUp].Rx = q.x();
+			rigidBodiesIndex[lookUp].Ry = q.y();
+			rigidBodiesIndex[lookUp].Rz = q.z();
 		
-		world.push(rigidBodiesIndex[lookupID]);
+			world.push(rigidBodiesIndex[lookUp]);
+		}
+		catch(err){
+			console.log(err)
+			console.log(lookUp)
+			delete rigidBodiesIndex[lookUp]}
 	}
 
 	/*IMPORTANT: See SO link above.  Can't send rigidBodiesIndex directly, had to copy to new array.  */
 	io.emit('setup', world);
+
+}
+
+function AddAPlayer(uniqueID){
+		//random start position for new player
+		//create random location for our tower, near other blocks
+	   var randX =  Math.floor(Math.random() * 20) - 10;
+	   var randZ =  Math.floor(Math.random() * 20) - 10;
+	
+		var cubeObjBlueprint = {
+			width : 2,
+			height : 2,
+			depth : 2,
+			mass : 5,
+			shape:'box',
+			color: Math.random() * 0xffffff,
+			x: randX,
+			y: 10,
+			z: randZ,
+			Rx: 0,
+			Ry: 0,
+			Rz: 0
+		}
+		
+
+		//build the object
+		var cube = createPhysicalCube(cubeObjBlueprint);
+
+		//add to our physics object holder
+		rigidBodies.push( cube.physics );
+		
+		//add to physics world
+		physicsWorld.addRigidBody( cube.physics );
+		
+		//add to our index used to update clients about objects that have moved
+		/*IMPORTANT: AddToRigidBodiesIndex expects that obj.physics is an Ammo object.  NOT the values sent used in the blueprint to build the object*/
+		AddToRigidBodiesIndex(cube);
+		
+		//associate the player ID with it's object in rigidBodies
+		PlayerIndex[uniqueID] =  cube.id;	
+		
+		//add player to worlds of other players and self
+		io.emit('newPlayer', {[uniqueID]:rigidBodiesIndex[cube.id]});
+
+}
+
+function RemoveAPlayer(uniqueID){
+	
+	var RB_id = PlayerIndex[uniqueID];
+	
+	//we will be splicing ridgidBodies so save initial
+	var count = rigidBodies.length;
+	
+	//remove from our rigidbodies holder
+	for(var i=0;i < count;i++){
+		//the construction of 'ids' in this whole server setup is WACKED! can lead to major headachs.  fix at some point
+		if(RB_id === 'id'+rigidBodies[i].ptr.toString() ){
+			console.log("REMOVING:", RB_id)
+			//remove player from the physical world
+			physicsWorld.removeRigidBody( rigidBodies[i] );
+			//remove player from rigidbodies
+			rigidBodies.splice(i,1);
+		}
+	}
+	
+	//remove from our rigidbodies index
+	delete rigidBodiesIndex[RB_id]
+	
+	//remove from player inded
+	delete PlayerIndex[uniqueID]
+	
+	//tell everyone to delete players cube
+	io.emit('removePlayer', RB_id);
 
 }
 
@@ -307,19 +435,37 @@ app.get('/', function(request, response){
 
 //socketio listener for 'connection'
 io.on('connection', function(socket){
-
+	
+	
+	socket.on('disconnect', function(){
+		console.log('playerleft: ', this.id)
+		RemoveAPlayer(this.id);
+	});
+	
+	//log
 	console.log('new user');
-	io.emit('connect','welcome');
+	console.log('session ID: ',socket.id);
+	
+	//send the new connection their uniqueID, which happens to be their socketID
+	io.to(socket.id).emit('playerID', socket.id);
+	
+	//create a player instance
+	AddAPlayer(socket.id);
+	
+	//get current state of everything, build specs and send out		
 	BuildWorldStateForNewConnection();
 	
-	//begin physics sim
+	//on first connection begin physics sim
 	if(!PhysicsSimStarted){
 		render();
 	}
+	
 	PhysicsSimStarted = true;
+	
 });
+
 
 http.listen(port, function(){
 	console.log('listening on port: '+port);
-	console.log('serving files from: '+__dirname);
+	console.log('serving files from root: '+__dirname);
 	});		
--- static/js/multiplayer_game1.js
+++ static/js/multiplayer_game1.js
@@ -5,7 +5,7 @@ var newPlayer = true;
 var rigidBodiesLookUp = {};
 var mouse = new THREE.Vector2();
 var clock = new THREE.Clock();
-
+var UNIQUE_ID; //assigned by the server
 
 //MAIN
 init();// start world building
@@ -17,27 +17,54 @@ var socket = io();
 		
 		socket.on('connect',function(msg){
 			connection = true;
+			socket.emit('move','test');
+		});
+		
+		socket.on('newPlayer',function(msg){
+
+			//don't build player if server is talking about you!
+			var NewID = Object.keys(msg)[0];
 			
+			if( NewID === UNIQUE_ID){
+			}else{createBoxObject(msg[NewID])}
+		});
+		
+		
+		socket.on('playerID',function(msg){
+			//server assigned uniqueID
+			UNIQUE_ID = msg;
 		});
 		
 		socket.on('setup', function(msg){
-			//msg should be JSON with each root key the ID of an object
+			console.log(msg)
+			//msg is an array of JSON with each root key the ID of an object
 			if(newPlayer){
 				//msg is the array of objects
 				for(var i =0; i<msg.length;i++){
 					
-					createBoxObject(msg[i]);
-					
+					if(msg[i].shape === 'box'){
+						createBoxObject(msg[i]);
+						}					
 					};
 					
-				newPlayer = false;
+				newPlayer = false;//prevent response to 'setup' msg intended for other players
 			};
 			animate();
+			
 		});
 		
 		socket.on('update', function(msg){
-			//msg should be JSON with each root key the ID of an object
+			//msg is a JSON with each root key the ID of an object and props x,y,z,Rx,Ry,Rz used to update the objects position/orientation in world
 			updateObjectLocations(msg);
+		});
+		
+
+		socket.on('removePlayer', function(msg){
+			//msg is an ID for an object
+			//remove it
+			console.log('remove: ',msg)
+			scene.remove( rigidBodiesLookUp[msg] )
+			delete rigidBodiesLookUp[msg];
 		});
 /*******************************/
 
@@ -98,13 +125,13 @@ function createBoxObject(object) {
 		var material;//consider passing mat types to flag basic, phong, etc...
 		
 		//http://threejs.org/docs/api/extras/geometries/BoxGeometry.html
-		var geometry = new THREE.BoxGeometry(object.w, object.d, object.h );
+		var geometry = new THREE.BoxGeometry(object.w, object.h, object.d );
 	
 		try{
-	   	material = new THREE.MeshBasicMaterial( { color: object.color} );}
+			material = new THREE.MeshBasicMaterial( { color: object.color} );}
 	   catch (err) {
-	   	console.log('no color property passed')
-	   	material = new THREE.MeshBasicMaterial( { color: "rgb(30%, 30%, 40%)"} );}
+			console.log('no color property passed')
+			material = new THREE.MeshBasicMaterial( { color: "rgb(30%, 30%, 40%)"} );}
 	   	
 		//http://threejs.org/docs/#Reference/Objects/Mesh
 		var Cube = new THREE.Mesh(geometry, material);
@@ -116,7 +143,7 @@ function createBoxObject(object) {
 	    Cube.quaternion.set(object.Rx, object.Ry, object.Rz,1 );
 	
 		//used to quickly find our object in our object array
-	   rigidBodiesLookUp[object.id] = Cube;
+	    rigidBodiesLookUp[object.id] = Cube;
 		
 		//add cube to graphics world
 		scene.add( Cube );
@@ -126,29 +153,34 @@ function createBoxObject(object) {
 
 function initInput() {
     controls = new THREE.OrbitControls( camera );
-	 controls.target.y = 2;
+	controls.target.y = 2;
 };
 
 function updateObjectLocations(updateJson){
 		
 		//IDs is an array of stings which are the IDs of objects in physics sim
 		//that can be matched up with their representation in our graphic objects tree rigidBodiesLookUp
-		
 		var IDs = Object.keys(updateJson);
-
+		console.log(IDs);
 		//cycle through objects that need an update
 		for(var i=0;i<IDs.length;i++){
-            
-         
-            //find the object
-			   var object = rigidBodiesLookUp[IDs[i]];
+			//get the objects ID
+			var id = IDs[i]
+			
+			try{
+				//find the object
+				var object = rigidBodiesLookUp[id];
 		
 				//get the new position/orientation for the object
-			  	var update = updateJson[IDs[i]];
+				var update = updateJson[id];
 		
-		     //apply update
-			  object.position.set( update.x,update.y,update.z);
-			  object.quaternion.set( update.Rx,update.Ry, update.Rz,1);	
+				//apply update
+				object.position.set( update.x,update.y,update.z);
+				object.quaternion.set( update.Rx,update.Ry, update.Rz,1);	
+			}
+			catch(err){console.log(rigidBodiesLookUp)
+				delete rigidBodiesLookUp[id];
+			}
 		}
 		
 }
@@ -163,7 +195,7 @@ function animate() {
 function render() {
 	   var deltaTime = clock.getDelta();
        renderer.render( scene, camera );//graphics
-	    controls.update( deltaTime );//view control
+	   controls.update( deltaTime );//view control
     };
     
     
